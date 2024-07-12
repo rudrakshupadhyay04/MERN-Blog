56ce8fdb02662dae0457d820b8611f98d15e0b54
rudrakshupadhyay04 <149106390+rudrakshupadhyay04@users.noreply.github.com>
2024-07-12 15:32:18 +0530

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from 'cookie-parser';
+import postRoutes from './routes/post.route.js';
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ app.listen(3000, ()=>{
 
 app.use('/api/user' , userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 // middleware
 app.use((err, req, res, next) => {
