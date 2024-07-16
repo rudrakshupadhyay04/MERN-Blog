cbf901887d038b88777046d36be135119cb2d1dd
rudrakshupadhyay04 <149106390+rudrakshupadhyay04@users.noreply.github.com>
2024-07-16 16:21:08 +0530

    add user functionality to the admin user

--- client/src/Components/DashUsers.jsx
+++ client/src/Components/DashUsers.jsx
@@ -49,19 +49,20 @@ export default function DashUsers() {
   };
 
   const handleDeleteUser = async () => {
-//     setShowModal(false);
-//     try {
-//       const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`, { method: 'DELETE'},);
-
-//       const data = await res.json();
-//       if(!res.ok){
-//         console.log(data.message);
-//       }else{
-//         setUserPosts((prev) => prev.filter((post) => post._id !== postIdToDelete));
-//       }
-//     } catch (error) {
-//         console.log(error.message);
-//     }
+    setShowModal(false);
+    try {
+      const res = await fetch(`/api/user/delete/${userIdToDelete}`, { method: 'DELETE'},);
+      const data = await res.json();
+      
+      if(!res.ok){
+        console.log(data.message);
+      }else{
+        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+        setShowModal(false);
+      }
+    } catch (error) {
+        console.log(error.message);
+    }
   };
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
