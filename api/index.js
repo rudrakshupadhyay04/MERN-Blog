3a80cffc54d0bf48366c582212ba3bcf572c13c2
rudrakshupadhyay04 <149106390+rudrakshupadhyay04@users.noreply.github.com>
2024-07-23 21:59:00 +0530

    add pre installaton

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import cookieParser from 'cookie-parser';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 });
 
+const __dirname = path.resolved()
 
 const app = express();
 
@@ -33,6 +35,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 // middleware
 app.use((err, req, res, next) => {
